adb9b8cba29e0be4b96371550045d06e65d5c6d5
maria1625 <166460177+maria1625@users.noreply.github.com>
2026-03-17 13:34:42 -0500

    feat: implementación de autenticación, navbar dinámico y rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
+     <Navbar />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+        <Route path="/" element={<LoginPage />} />
+         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+       </Routes>
     </BrowserRouter>
   );
 }
